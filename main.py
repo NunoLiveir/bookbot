1dcea1d00b98f67551f7d57daf5e0a6ad35d1fde
NunoLiveir <nunoliveira_3@hotmail.com>
2025-11-20 12:08:54 +0000

    Added get_book_text

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    """Return the entire contents of the file as a string."""
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def main():
+     text = get_book_text("books/frankenstein.txt")
+     print(text)
+
+main()
