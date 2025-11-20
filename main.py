a05da9efe42921ebbf004a80eae64975e8754912
NunoLiveir <nunoliveira_3@hotmail.com>
2025-11-20 12:27:43 +0000

    Created new stats.py for refactoring

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from stats import count_words
+
+
+
 def get_book_text(filepath):
     """Return the entire contents of the file as a string."""
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
 
+
 def main():
      text = get_book_text("books/frankenstein.txt")
-     print(text)
+     print(f"Found {count_words(text)} total words")
+     
 
 main()
